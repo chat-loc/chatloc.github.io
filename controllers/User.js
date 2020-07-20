20c6fa23e0e07898059f02c7a09f5d950893f660
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-07-20 19:06:24 -0400

    Implement form validatio for login form

--- controllers/User.js
+++ controllers/User.js
@@ -29,26 +29,66 @@ router.post("/registration", (req, res) => {
 
 router.post("/login", (req, res) => { 
 
-	const name = req.body.name;
+	// Function to check for nulls
+	const checkNull = (key, field, errors, loginVals) => {
+	    (field == "") ? errors.null[`${key}`] = ' should not be empty' : loginVals[`${key}`] = field;
+	};
+
+	// Fetch the details
+	const name = (req.body.name).trim();
 	const password = req.body.password;
 
-	// Check if login details exist in DB
+	// Create object to hold errors
+	let errors = {
+	    null : {},
+	    regex : {}
+	};
+	let loginVals = {};
+	let formValid = false;
 
-	userModel.findOne({
+	// Check if user enters nothing
+	checkNull ("name", name, errors, loginVals);
+	checkNull ("password", password, errors, loginVals);
 
-		name : name,
-		password : password
+	// If errors for invalid patterns exist, re-render route to referring page and export errors object
+	if (Object.keys(errors.null).length > 0) {
+	    formValid = false;
 
-	}, function (err, login) {
+	    console.log(errors);
+	    console.log(loginVals);
 
-		if (err || !login) {
-			console.log (err);
-			res.redirect("/user/login");
-		} else {
-			res.redirect("/");
-		}
+	    res.render('User/login', {
+	        errors : errors.null,
+	        loginVals,
+	        errorClass : {active: "active"},
+	    });
 
-	});
+	}    // Otherwise redirect (and reload) Home page
+     else {
+     	console.log(errors);
+
+     	// Check if login details exist in DB
+     	userModel.findOne({
+
+     		name : name,
+     		password : password
+
+     	}, function (err, login) {
+
+     		if (err || !login) {
+     			console.log (err);
+     			res.render('User/login', {
+     			    errors : errors.null,
+     			    loginVals,
+     			    errorClass : {active: "active"},
+     			});
+     		} else {
+     			console.log ('Login successful');
+     			res.redirect("/");
+     		}
+
+     	});        
+    }
 
 });
 
