14ee1f14bdef0bffae3e00edd0a5e25908074db2
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-07-20 15:33:18 -0400

    Save users to MongoDB Cloud

--- controllers/User.js
+++ controllers/User.js
@@ -9,6 +9,27 @@ router.get("/registration", (req, res) => {
     res.render("User/registration");
 });
 
+//Route to process user's request and data when user submits registration form
+router.post("/registration", (req, res) => { 
+
+	const newUser = {
+		name : req.body.name, 
+		email : req.body.email,
+		password : req.body.password,
+		origin : req.body.origin,
+		sex : req.body.sex
+	}
+
+	console.log(req.body);
+
+	const user = new userModel(newUser);
+	user.save().then(() => {
+		res.redirect("/")
+	}).catch(err => console.log(`Error while inserting into the data ${err}`));
+ 
+});
+
+
 //Route to direct use to Registration form
 router.get("/login", (req, res) => {
     res.render("User/login");
