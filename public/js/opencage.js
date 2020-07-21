7c1a26bab72ed51f9c063c6411a85879494a1d79
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-07-21 16:17:00 -0400

    Handle success and error callbacks of geolocation

--- public/js/opencage.js
+++ public/js/opencage.js
@@ -1,11 +1,41 @@
 
+	// User accepts to share location
+	const successCallback = (position) => {
+		console.log(position);
+		$appClose.classList.add('allowed');
 
-	
-	geolocate () => {
-		const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
+
+		/*
+		GeolocationPosition: {
+			coords: GeolocationCoordinates
+			accuracy: 3642
+			altitude: null
+			altitudeAccuracy: null
+			heading: null
+			latitude: 43.7257028
+			longitude: -79.59404339999999
+			speed: null
+		}
+		*/
 	}
 
-	closeModal () => {
+	// User declines to share location
+	const errorCallback = (error) => {
+		console.log(error);
+		$page.style.display = 'none';
+		$dialogModal.style.display = 'none';
+	}
+	
+	// Get location
+	const geolocate = () => {
+		const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, {
+			enableHighAccuracy : true, 	//try best to provide with a high accurate location
+			timeout: 5000
+		});	
+		 //$appClose.classList.add('allowed');	
+	}
+
+	async function closeModal () {
 		$appClose.addEventListener('click', () => {
 			$dialogModal.classList.add('hide');
 		});
@@ -13,7 +43,9 @@
 	
 	const $appClose = document.querySelector('#app-alert-close');
 	const $dialogModal = document.querySelector('#dialog-app-modal');
+	const $page = document.querySelector('.main-wrapper');
 
+	geolocate();
 
 
 
