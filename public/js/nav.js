922de847fd2af2572314da9e0868a57feb51fd92
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-08-08 01:09:38 -0400

    Archive directories and create new client, server dirs to prepare react

--- public/js/nav.js
+++
@@ -1,9 +0,0 @@
-   	
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FyeW91cmRheSIsImEiOiJja2Npb2JoM2wwYnFkMnFvNWJmYzF2ZGN6In0.vJd1qEXRZXV8tNuwo1l4LQ';
-
-	var map = new mapboxgl.Map({
-		container: 'map', // container id
-		style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
-		center: [-79.5132, 43.6205], // starting position
-		zoom: 13 // starting zoom
-	});
