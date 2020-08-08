268c422a69567f1b8fbc8c50b8d881e1ffffd234
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-08-08 11:54:06 -0400

    Import font and night mode styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+// Import stylesheets
 import "./styles/index.css";
+import "./styles/font.css";
+import "./styles/nightMode.css";
 
 import Login from './components/Login/Login';
 
