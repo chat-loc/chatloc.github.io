e2715659f1a3e5ca0642b9e57651dcaeecb8f656
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-08-11 16:17:43 -0400

    Prepare server file for bi-dir communication

--- server/index.js
+++ server/index.js
@@ -9,8 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Set up socket
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 // BodyParser middleware
 app.use(bodyParser.urlencoded({extended:false}));
+//express static middleware
+app.use(express.static("public"));
+
 const Schema = mongoose.Schema;
 
 
@@ -32,6 +39,37 @@ mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true,
 const PORT = process.env.PORT || 5003;
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
+});
+
+
+// PREPARE CHAT SCHEMA
+const chatSchema = new Schema({
+    name : {
+        type : String
+    },
+    msg : {
+        type : String
+    },
+    room : {
+        type : String
+    },
+    dateCreated : {
+        type : Date,
+        default : Date.now()
+    }
+});
+
+/*For every Schema you create (create a schema per collection), you must also create a model
+The model will allow you to perform CRUD operations on a given collection*/
+
+const mongoChat = mongoose.model('Chat', chatSchema);
+
+const tech = io.of('/tech');
+const users = {};
+
+tech.on('connection', function (socket) {
+
+
 });
