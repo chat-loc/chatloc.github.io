559273394b3dce7aa76b914e8a096e670aa8c6f4
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-08-17 16:55:14 -0400

    Reposition server 'listen'

--- index.js
+++ index.js
@@ -62,11 +62,6 @@ mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true,
 const PORT = process.env.PORT || 5003;
 
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
-
-
 // PREPARE CHAT SCHEMA
 const locChatSchema = new Schema({
     name : {
@@ -180,4 +175,9 @@ io.on('connection', function (socket) {
 	});
 
 
+});
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });
